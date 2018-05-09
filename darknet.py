fix bug in detection index computation

darknet.py
@@ -276,6 +276,16 @@ class Darknet(nn.Module):
         anchors = np.array([(a[0], a[1]) for layer_a in anchors for a in layer_a])
         anchors = anchors[np.newaxis, :]
 
+        # size of the tensor for each yolo layer
+        yolo_ts_array = np.array([tensor_size[x] for x, _ in yolo_modules])
+        #res_start_array = np.zeros(num_yolo_layers + 1)
+        #res_start_array[1:] = np.cumsum(num_anchors * yolo_ts_array)
+        # number of boxes in each yolo layer
+        yolo_num_box_array = [ts * ts * n for ts, n in zip(yolo_ts_array, num_anchors)]
+        yolo_stride_array = [inp_dim / ts for ts in yolo_ts_array]
+        stride_long_array = np.hstack(([stride] * num_boxes
+            for stride, num_boxes in zip(yolo_num_box_array, yolo_stride_array)))
+
         batch_size = detections.shape[0]
         num_detections = detections.shape[1]
         num_classes = detections.shape[2] - 5
@@ -321,18 +331,16 @@ class Darknet(nn.Module):
             for q, j, gt_elem in zip(range(anchor_idx.size), anchor_idx, img_gt):
                 # index of the yolo layer with the best anchor
                 layer_idx = np.searchsorted(num_anchors, j, side='right')
-                # index of the module (layer) with the best anchor
-                module_idx = yolo_modules[layer_idx][0]
                 # get index of the cell corresponding to the truth box
-                ts = tensor_size[module_idx]
-                stride = inp_dim / ts
+                ts = yolo_ts_array[layer_idx]
+                stride = yolo_stride_array[layer_idx]
                 cell_x = gt_elem[0] // stride
                 cell_y = gt_elem[1] // stride
                 # index of the bounding box in detections
                 box_idx = 0
-                for k in range(module_idx):
+                for k in range(layer_idx):
                     # size of the yolo layer
-                    ts_k = tensor_size[yolo_modules[k][0]]
+                    ts_k = yolo_ts_array[k]
                     # skip detections of previous yolo layers
                     box_idx += num_anchors[k] * ts_k * ts_k
                 # linear index of the cell